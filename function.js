9b750dc66f31ca1300075e61a03fcc26bca730f9
spellcasterfrom9ja <120885394+spellcasterfrom9ja@users.noreply.github.com>
2022-12-25 01:25:10 -0800

    Add files via upload

--- function.js
+++ function.js
@@ -2,8 +2,11 @@ var i = 0;
 var images = [];
 var time = 2000;
 
-images[0] = 'banner1.png';
-images[1] = 'banner2.png';
+images[0] = 'IMG-20221223-WA0008.jpg';
+images[1] = 'IMG-20221223-WA0011.jpg';
+images[2] = 'IMG-20221223-WA0021.jpg';
+images[3] = 'IMG-20221223-WA0026.jpg';
+images[4] = 'IMG-20221223-WA0027.jpg';
 
 function changeImg(){
     document.slide.src = images[i];
